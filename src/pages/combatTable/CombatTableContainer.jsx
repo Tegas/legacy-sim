3315e85af50294c44399579e63e3367e9e4166a2
tegas <29030328+Tegas@users.noreply.github.com>
2019-07-20 17:38:34 -0400

    adding attack types to attack table

--- src/pages/combatTable/CombatTableContainer.jsx
+++ src/pages/combatTable/CombatTableContainer.jsx
@@ -8,99 +8,147 @@ class CombatTableContainer extends Component {
   componentWillMount() {
     this.props.initialize({
       skill: 300,
-      hit: 8,
+      hit: 5,
       crit: 10,
       attackerLevel: 60,
       targetLevel: 63,
-      behind: true,
+      hasParry: true,
+      hasBlock: false,
     });
   }
 
-  computeCombatTable() {
+  /**
+   * Puts the number in a range of zero to one hundred.
+   *
+   * @param {number} number - The number.
+   */
+  normalize(number) {
+    return Math.max(Math.min(number, 100), 0);
+  }
+
+  computeChances(attackType = 'auto') {
 
     // https://github.com/magey/classic-warrior/wiki/Attack-table
+    // https://web.archive.org/web/20070222175829/http://forums.wow-europe.com/thread.html?topicId=166546791&pageNo=1&sid=1
 
     const skill = +this.props.formValues.skill;
     const hit = +this.props.formValues.hit;
     const crit = +this.props.formValues.crit;
     const attackerLevel = +this.props.formValues.attackerLevel;
     const targetLevel = +this.props.formValues.targetLevel;
-    const behind = !!this.props.formValues.behind;
-
-    
-    let hitChance = 0.0;
-    let critChance = 0.0;
-    let dodgeChance = 0.0;
-    let missChance = 3.0;
-    let parryChance = 14.0;
-    let glancingChance = 0.0;
-    
-    
+    const hasParry = !!this.props.formValues.hasParry;
+    const hasBlock = !!this.props.formValues.hasBlock;
     const defense = targetLevel * 5;
     const baseWeaponSkill = Math.min(300, skill);
 
-    if (baseWeaponSkill - defense < 0) {
-      critChance = crit + (baseWeaponSkill - defense) * 0.2;
-    } else {
-      critChance = crit + (baseWeaponSkill - defense) * 0.4;
-    }
-
-    dodgeChance = Math.max(5 + (targetLevel * 5 - skill) * 0.1, 0);
-
+    let missChance = attackType === 'dual' ? 19.0 : 0.0;
     if (defense - skill > 11) {
-      missChance = 5 + (targetLevel * 5 - skill) * 0.2;
+      missChance += 5 + (targetLevel * 5 - skill) * 0.2;
     } else {
-      missChance = 5 + (targetLevel * 5 - skill) * 0.1;
+      missChance += 5 + (targetLevel * 5 - skill) * 0.1;
     }
     // low level bonus
-    missChance *= (Math.min(10, targetLevel) / 10);
+    missChance *= (Math.min(10, targetLevel) / 10); // Note, unsure if this affects dual weild... but you can't duel wield < lvl 10
 
+    // Skill difference penalty
     if (defense - skill > 10 && hit >= 1) {
       missChance -= (hit - 1);
     } else {
       missChance -= hit;
     }
-    missChance = Math.max(missChance, 0);
 
-    glancingChance = Math.max(10 + (targetLevel * 5 - Math.min(attackerLevel * 5, skill)) * 2.0, 0);
-
+    const dodgeChance = Math.max(5 + (targetLevel * 5 - skill) * 0.1, 0);
+    const critChance = crit + (baseWeaponSkill - defense) * (baseWeaponSkill - defense < 0 ? 0.2 : 0.4) - 1.8;
+    const parryChance = hasParry ? Math.max(5.0 + ((defense - skill) * 0.6), 0) : 0.0
+    const blockChance = hasBlock ? Math.min(5.0, 5 + (targetLevel * 5 - skill) * 0.1) : 0.0;
+    const glancingChance = attackType === 'special' ? 0.0 : 10 + (targetLevel * 5 - Math.min(attackerLevel * 5, skill)) * 2.0;
     const lowend = Math.min(1.3 - 0.05*(defense - skill), 0.91);
     const highend = Math.max(Math.min(1.2 - 0.03*(defense - skill), 0.99), 0.2);
-    const glancingPenalty = (1 - ((lowend + highend) / 2)) * 100;
-
-    parryChance = behind ? 0.0 : Math.max(5.0 + ((defense - skill) * 0.6), 0);
-
-    hitChance = 100 - dodgeChance - missChance - parryChance - glancingChance;
-    critChance = Math.max(Math.min(hitChance, critChance), 0);
-    hitChance = hitChance - critChance;
-
-    const overall = +hitChance + (2.0 * critChance) + (glancingChance * (1 - (glancingPenalty / 100)));
+    const glancingPenalty = attackType === 'special' ? 0.0 : (1 - ((lowend + highend) / 2)) * 100;
 
     return {
-      hitChance: hitChance.toFixed(2),
-      critChance: critChance.toFixed(2),
-      dodgeChance: dodgeChance.toFixed(2),
-      missChance: missChance.toFixed(2),
-      parryChance: parryChance.toFixed(2),
-      glancingChance: glancingChance.toFixed(2),
-      glancingPenalty: glancingPenalty.toFixed(0),
-      overall: overall.toFixed(2),
-      pieChart: [
-        { name: 'Hit', value: hitChance },
-        { name: 'Crit', value: critChance },
-        { name: 'Dodge', value: dodgeChance },
-        { name: 'Miss', value: missChance },
-        { name: 'Parry', value: parryChance },
-        { name: 'Glancing', value: glancingChance },
-      ], 
+      critChance,
+      dodgeChance,
+      missChance,
+      parryChance,
+      blockChance,
+      glancingChance,
+      glancingPenalty,
     }
   }
+
+  computeAttackTable({
+    critChance,
+    dodgeChance,
+    missChance,
+    parryChance,
+    blockChance,
+    glancingChance,
+    glancingPenalty,
+  }) {
+
+    let remaining = 100.0;
+    const miss = this.normalize(missChance);
+    remaining -= miss;
+
+    const dodge = Math.min(this.normalize(dodgeChance), remaining);
+    remaining -= dodge;
+
+    const parry = Math.min(this.normalize(parryChance), remaining);
+    remaining -= parry;
+
+    const block = Math.min(this.normalize(blockChance), remaining);
+    remaining -= block;
+
+    const glancing = Math.min(this.normalize(glancingChance), remaining);
+    remaining -= glancing;
+
+    const crit = Math.min(this.normalize(critChance), remaining);
+    remaining -= crit;
+
+    const hit = remaining;
+
+    const overHitCap = this.normalize(missChance * -1);
+    const overCritCap = this.normalize(critChance - crit);
+    const overall = +hit + (2.0 * crit) + (glancing * (1 - (glancingPenalty / 100)));
+
+    return {
+      hitChance: hit.toFixed(2),
+      critChance: crit.toFixed(2),
+      dodgeChance: dodge.toFixed(2),
+      missChance: miss.toFixed(2),
+      parryChance: parry.toFixed(2),
+      blockChance: block.toFixed(2),
+      glancingChance: glancing.toFixed(2),
+      glancingPenalty: glancingPenalty.toFixed(0),
+      overHitCap: overHitCap.toFixed(2),
+      overCritCap: overCritCap.toFixed(2),
+      overall: overall.toFixed(2),
+      pieChart: [
+        { name: 'Miss', value: miss },
+        { name: 'Dodge', value: dodge },
+        { name: 'Parry', value: parry },
+        { name: 'Block', value: block },
+        { name: 'Glancing', value: glancing },
+        { name: 'Crit', value: crit },
+        { name: 'Hit', value: hit },
+      ],
+    }
+  }
+
+  computeAttackTables() {
+    return {
+      auto: this.computeAttackTable(this.computeChances('auto')),
+      special: this.computeAttackTable(this.computeChances('special')),
+      dual: this.computeAttackTable(this.computeChances('dual')),
+    };
+  };
 
   render() {
     return (
       <CombatTableView
         formValues={ this.props.formValues }
-        combatTable={ this.computeCombatTable() }
+        attackTables={ this.computeAttackTables() }
       />
     );
   }
--- src/pages/combatTable/CombatTableView.jsx
+++ src/pages/combatTable/CombatTableView.jsx
@@ -3,122 +3,182 @@ import PropTypes from 'prop-types';
 import { Field } from 'redux-form';
 import { NavLink } from 'react-router-dom';
 import {
-  ResponsiveContainer,
   PieChart,
   Pie,
-  Tooltip,
+  Cell,
 } from 'recharts';
+import './combatTable.css';
+
+
+const dataz = [{name: 'Group A', value: 400}, {name: 'Group B', value: 300},
+                  {name: 'Group C', value: 300}, {name: 'Group D', value: 200}];
+const COLORS = ['OrangeRed', 'purple', 'MediumBlue', 'teal', 'olive', 'black', 'gray'];
 
 const CombatTableView = ({
-  combatTable,
+  attackTables
 }) => (
-  <div>
     <div>
-      <div className='row columns'>
-        <nav aria-label='You are here:'>
-          <ul className='breadcrumbs'>
-            <li><NavLink to="/">Home</NavLink></li>
-            <li><span className='show-for-sr'>Current:</span> Combat Table</li>
-          </ul>
-        </nav>
-      </div>
-      <div className='row'>
-        <h3>Combat Table</h3>
-        <h2>Work In progress...</h2>
-      </div>
-      <div className='row'>
-        
-        <div className='medium-4 large-5 columns'>
-          <div className='row'>
-            <div className='large-6 columns'>
-              <label htmlFor='attackerLevel'>Attacker Level
-                <Field name='attackerLevel' component='input' type='number' min='1' max='63' />
-              </label>
-            </div>
-            <div className='large-6 columns'>
-              <label htmlFor='targetLevel'>Target Level
+      <div>
+        <div className='row columns'>
+          <nav aria-label='You are here:'>
+            <ul className='breadcrumbs'>
+              <li><NavLink to="/">Home</NavLink></li>
+              <li><span className='show-for-sr'>Current:</span> Attack Table</li>
+            </ul>
+          </nav>
+        </div>
+        <div className='row'>
+          <h3>Attack Table</h3>
+        </div>
+        <div className='row'>
+          <div className='large-6 columns end'>
+            <div className='row'>
+              <div className='small-6 medium-4 columns'>
+                <label htmlFor='attackerLevel'>Player Level
+                <Field name='attackerLevel' component='input' type='number' min='1' max='60' />
+                </label>
+              </div>
+              <div className='small-6 medium-4 columns end'>
+                <label htmlFor='targetLevel'>Target Mob Level
                 <Field name='targetLevel' component='input' type='number' min='1' max='63' />
-              </label>
-            </div>
-          </div>
-          <div className='row'>
-            <div className='large-12 columns'>
-              <label htmlFor='hit'>Hit %
-                <Field name='hit' component='input' type='number' min='0' max='999' />
-              </label>
-            </div>
-          </div>
-          <div className='row'>
-            <div className='large-12 columns'>
-              <label htmlFor='crit'>Crit %
-                <Field name='crit' component='input' type='number' min='0' max='999' />
-              </label>
-            </div>
-          </div>
-          <div className='row'>
-            <div className='large-12 columns'>
-              <label htmlFor='skill'>Weapon Skill
-                <Field name='skill' component='input' type='number' min='0' max='999' />
-              </label>
-            </div>
-          </div>
-          <div className='row'>
-            <div className='large-12 columns'>
-                <Field name='behind' id='behind' component='input' type='checkbox' />
-                <label htmlFor='behind'>
-                  <span data-tooltip aria-haspopup='true' className='has-tip tip-bottom' title='Attacking from behind ignores parry.'>
-                    Attack from behind
-                  </span>
                 </label>
               </div>
             </div>
+            <div className='row'>
+              <div className='small-4 columns'>
+                <label htmlFor='hit'>Hit %
+                <Field name='hit' component='input' type='number' min='0' max='999' />
+                </label>
+              </div>
+              <div className='small-4 columns'>
+                <label htmlFor='crit'>Crit %
+                <Field name='crit' component='input' type='number' min='0' max='999' />
+                </label>
+              </div>
+              <div className='small-4 columns end'>
+                <label htmlFor='skill'>Skill
+                <Field name='skill' component='input' type='number' min='0' max='999' />
+                </label>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className='medium-8 large-7 columns'>
-            <PieChart width={240} height={240}>
-              <Pie label nameKey="name" dataKey="value" isAnimationActive={false} data={combatTable.pieChart} outerRadius={80} fill="#8884d8" label />
-              <Tooltip isAnimationActive={ false } />
-            </PieChart>
-          <div className='row'>
-            <div className='column large-12'>
-              <table>
-                <tbody>
-                  <tr>
-                    <th>Hit</th>
-                    <td>{combatTable.hitChance}%</td>
-                  </tr>
-                  <tr>
-                    <th>Crit</th>
-                    <td>{combatTable.critChance}%</td>
-                  </tr>
-                  <tr>
-                    <th>Miss</th>
-                    <td>{combatTable.missChance}%</td>
-                  </tr>
-                  <tr>
-                    <th>Dodge</th>
-                    <td>{combatTable.dodgeChance}%</td>
-                  </tr>
-                  <tr>
-                    <th>Parry</th>
-                    <td>{combatTable.parryChance}%</td>
-                  </tr>
-                  <tr>
-                    <th>Glancing</th>
-                    <td>{combatTable.glancingChance}% ({combatTable.glancingPenalty}% penalty)</td>
-                  </tr>
-                  <tr>
-                    <th>Overall</th>
-                    <th>{combatTable.overall}% damage</th>
-                  </tr>
-                </tbody>
-              </table>
+        <div className='row'>
+          <div className='columns'>
+            <div className='row'>
+              <div className='column large-12'>
+                <table className="combat-table">
+                  <tbody>
+                    <tr>
+                      <th>
+                      </th>
+                      <th>
+                        <PieChart width={100} height={100}>
+                          <Pie nameKey="name" dataKey="value" isAnimationActive={false} data={attackTables.auto.pieChart} outerRadius={50}>
+                              { attackTables.auto.pieChart.map((entry, index) => <Cell fill={COLORS[index % COLORS.length]}/>) }
+                          </Pie>
+                        </PieChart>
+                      </th>
+                      <th>
+                        <PieChart width={100} height={100}>
+                          <Pie nameKey="name" dataKey="value" isAnimationActive={false} data={attackTables.special.pieChart} outerRadius={50}>
+                              { attackTables.special.pieChart.map((entry, index) => <Cell fill={COLORS[index % COLORS.length]}/>) }
+                          </Pie>
+                        </PieChart>
+                      </th>
+                      <th>
+                        <PieChart width={100} height={100}>
+                          <Pie nameKey="name" dataKey="value" isAnimationActive={false} data={attackTables.dual.pieChart} outerRadius={50}>
+                              { attackTables.dual.pieChart.map((entry, index) => <Cell fill={COLORS[index % COLORS.length]}/>) }
+                          </Pie>
+                        </PieChart>
+                      </th>
+                    </tr>
+                    <tr className="">
+                      <th></th>
+                      <td>Auto Attack Damage</td>
+                      <td>Special Attack Damage</td>
+                      <td>Dual Wield Damage</td>
+                    </tr>
+                    <tr className="combat-table_miss">
+                      <th>Miss</th>
+                      <td>{attackTables.auto.missChance}% {attackTables.auto.overHitCap > 0 && (
+                        <span className="combat-table_note">({attackTables.auto.overHitCap}% over hit cap)</span>
+                      )}</td>
+                      <td>{attackTables.special.missChance}% {attackTables.special.overHitCap > 0 && (
+                        <span className="combat-table_note">({attackTables.special.overHitCap}% over hit cap)</span>
+                      )}</td>
+                      <td>{attackTables.dual.missChance}% {attackTables.dual.overHitCap > 0 && (
+                        <span className="combat-table_note">({attackTables.dual.overHitCap}% over hit cap)</span>
+                      )}</td>
+                    </tr>
+                    <tr className="combat-table_dodge">
+                      <th>Dodge</th>
+                      <td>{attackTables.auto.dodgeChance}%</td>
+                      <td>{attackTables.special.dodgeChance}%</td>
+                      <td>{attackTables.dual.dodgeChance}%</td>
+                    </tr>
+                    <tr className="combat-table_parry">
+                      <th>
+                      <Field name='hasParry' id='hasParry' component='input' type='checkbox' /> Parry
+                      </th>
+                      <td>{attackTables.auto.parryChance}%</td>
+                      <td>{attackTables.special.parryChance}%</td>
+                      <td>{attackTables.dual.parryChance}%</td>
+                    </tr>
+                    <tr className="combat-table_block">
+                      <th>
+                      <Field name='hasBlock' id='hasBlock' component='input' type='checkbox' /> Block
+                      </th>
+                      <td>{attackTables.auto.blockChance}%</td>
+                      <td>{attackTables.special.blockChance}%</td>
+                      <td>{attackTables.dual.blockChance}%</td>
+                    </tr>
+                    <tr className="combat-table_glancing">
+                      <th>Glancing</th>
+                      <td>{attackTables.auto.glancingChance}% {attackTables.auto.glancingChance > 0 && (
+                        <span className="combat-table_note">({attackTables.auto.glancingPenalty}% penalty)</span>
+                      )}</td>
+                      <td>{attackTables.special.glancingChance}% {attackTables.special.glancingChance > 0 && (
+                        <span className="combat-table_note">({attackTables.special.glancingPenalty}% penalty)</span>
+                      )}</td>
+                      <td>{attackTables.dual.glancingChance}% {attackTables.dual.glancingChance > 0 && (
+                        <span className="combat-table_note">({attackTables.dual.glancingPenalty}% penalty)</span>
+                      )}</td>
+                    </tr>
+                    <tr className="combat-table_crit">
+                      <th>Crit</th>
+                      <td>{attackTables.auto.critChance}% {attackTables.auto.overCritCap > 0 && (
+                        <span className="combat-table_note">({attackTables.auto.overCritCap}% over)</span>
+                      )}</td>
+                      <td>{attackTables.special.critChance}% {attackTables.special.overCri > 0 && (
+                        <span className="combat-table_note">({attackTables.special.overCritCap}% over)</span>
+                      )}</td>
+                      <td>{attackTables.dual.critChance}% {attackTables.dual.overCritCap > 0 && (
+                        <span className="combat-table_note">({attackTables.dual.overCritCap}% over)</span>
+                      )}</td>
+                    </tr>
+                    <tr className="combat-table_hit">
+                      <th>Hit</th>
+                      <td>{attackTables.auto.hitChance}%</td>
+                      <td>{attackTables.special.hitChance}%</td>
+                      <td>{attackTables.dual.hitChance}%</td>
+                    </tr>
+                    <tr>
+                      <th>Overall</th>
+                      <th>{attackTables.auto.overall}% damage</th>
+                      <th>{attackTables.special.overall}% damage</th>
+                      <th>{attackTables.dual.overall}% damage</th>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
 
 CombatTableView.propTypes = {
   armorTable: PropTypes.arrayOf(PropTypes.any),
